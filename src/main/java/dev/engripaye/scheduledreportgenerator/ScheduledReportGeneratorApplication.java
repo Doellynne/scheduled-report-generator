d550534bd61ab2a358512eed4eec0086eff4956f
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-18 01:17:23 +0100

    code commit by ENGR. IPAYE

---
+++ src/main/java/dev/engripaye/scheduledreportgenerator/ScheduledReportGeneratorApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.scheduledreportgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledReportGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledReportGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engripaye/scheduledreportgenerator/ScheduledReportGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.scheduledreportgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduledReportGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
